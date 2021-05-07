dc157cbd321f8a320e51c69488c17c459f89815b
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2021-05-07 17:02:07 +0200

    Fix path

--- run_dcache.sh
+++ run_dcache.sh
@@ -42,7 +42,7 @@ ls -R "$TEMPWD"
 MACAROON="helloflow_macaroon.conf"
 ada --tokenfile ${MACAROON} --api https://dcacheview.grid.surfsara.nl:22880/api/v1 --whoami # Check access
 # ada --tokenfile ${MACAROON} --api https://dcacheview.grid.surfsara.nl:22880/api/v1 --mkdir "$OUTDIR" # Create the remote directory
-rclone --config=${MACAROON} copy helloflow_macaroon:/"$TEMPWD"/output/ "$OUTDIR" # Copy
+rclone --config=${MACAROON} copy "$TEMPWD"/output/ helloflow_macaroon:/"$OUTDIR" # Copy
 
 echo "SUCCESS"
 exit 0
